005d3fcc3c95c28a02fd9d001de5bd9226cb5ac8
Kévin - Noxtaria <noxtariadev@gmail.com>
2022-10-01 18:34:57 +0200

    Ajout Router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from "./App.vue"
+import VueRouter from 'vue-router'
+import IndexComponent from "./pages/Index.vue"
+import FormComponent from "./pages/Form.vue"
+import ResultComponent from "./pages/Result.vue"
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+    path: '/',
+    component: IndexComponent
+    },
+    {
+    path: '/form',
+    component: FormComponent
+    },
+    {
+    path: '/result',
+    component: ResultComponent
+    },
+    {
+    path: '*',
+    redirect: '/'
+    }
+
+  ]
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
