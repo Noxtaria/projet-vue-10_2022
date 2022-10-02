e4af358959bd44b8d896af037b1f2d910914491f
Kévin - Noxtaria <noxtariadev@gmail.com>
2022-10-02 19:00:32 +0200

    Ajout du composant Result dans l'Index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,15 +2,17 @@
   <div>
     <h1>Bienvenue sur l'index du projet Vue</h1>
 
-    
+    <Result />
   </div>
-  
 
-    
-  </template>
+
+
+</template>
   
-  <script>
-    export default {
-    name: "IndexComponent",
+<script>
+import Result from '@/components/Result.vue';
+export default {
+  name: "IndexComponent",
+  components: { Result }
 }
-  </script>
+</script>
