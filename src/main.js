cbba2c53b915da0bea7bf2fa349c1e15e1abc6d9
Kévin - Noxtaria <noxtariadev@gmail.com>
2022-10-03 18:06:18 +0200

    Init Vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 import PropsComponent from "./pages/Props.vue"
 import VueXComponent from "./pages/VueX.vue"
 import BulmaComponent from "./pages/Bulma.vue"
+import store from './store'
 
 Vue.use(VueRouter)
 
@@ -37,5 +38,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
